60091697d4d4a67f1ba951260a506d5911da5f64
SanchiJoshi <156744898+SanchiJoshi@users.noreply.github.com>
2025-06-05 23:51:25 +0530

    Time: 5 ms (61.11%) | Memory: 45.4 MB (39.19%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -3,13 +3,11 @@ class Solution {
        if (numRows == 1 || numRows >= s.length()) {
             return s;
         }
-
         int idx = 0, d = 1;
         List<Character>[] rows = new ArrayList[numRows];
         for (int i = 0; i < numRows; i++) {
             rows[i] = new ArrayList<>();
         }
-
         for (char c : s.toCharArray()) {
             rows[idx].add(c);
             if (idx == 0) {
@@ -19,14 +17,12 @@ class Solution {
             }
             idx += d;
         }
-
         StringBuilder result = new StringBuilder();
         for (List<Character> row : rows) {
             for (char c : row) {
                 result.append(c);
             }
         }
-
         return result.toString();        
     }
 }
